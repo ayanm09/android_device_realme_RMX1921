9e5ad5df5b05ff16c25d1c61fea47e918571c065
Ayan Mukherjee <mukherjeeayan725@gmail.com>
2020-12-29 13:00:42 +0530

    RMX1921: Superior Bringup

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,8 +5,8 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/aosp_RMX1921.mk
+    $(LOCAL_DIR)/superior_RMX1921.mk
 
 COMMON_LUNCH_CHOICES := \
-    aosp_RMX1921-userdebug \
-    aosp_RMX1921-eng
+    superior_RMX1921-userdebug \
+    superior_RMX1921-eng
--- superior_RMX1921.mk
+++ superior_RMX1921.mk
@@ -4,11 +4,12 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-# Inherit some common PE stuff
+# Inherit some common Superior stuff
 TARGET_GAPPS_ARCH := arm64
 TARGET_BOOT_ANIMATION_RES := 1080
 
-$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
+$(call inherit-product, vendor/superior/config/common.mk)
+$(call inherit-product-if-exists, vendor/pixelgapps/pixel-gapps.mk)
 
 # Inherit from RMX1921 device
 $(call inherit-product, $(LOCAL_PATH)/device.mk)
@@ -16,7 +17,7 @@ $(call inherit-product, $(LOCAL_PATH)/device.mk)
 PRODUCT_BRAND := Realme
 PRODUCT_DEVICE := RMX1921
 PRODUCT_MANUFACTURER := Realme
-PRODUCT_NAME := aosp_RMX1921
+PRODUCT_NAME := superior_RMX1921
 PRODUCT_MODEL := Realme XT
 
 PRODUCT_GMS_CLIENTID_BASE := android-realme
@@ -27,5 +28,5 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
         PRIVATE_BUILD_DESC="coral-user 11 RQ1A.201205.008 6943376 release-keys" \
         PRODUCT_NAME="RMX1921" \
 	TARGET_DEVICE="RMX1921"
-    
+
 
